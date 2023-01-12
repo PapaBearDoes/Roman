27193169e021990fa41bfeb04d5ba25fec5b2fc4
PapaBearDoes <73200436+PapaBearDoes@users.noreply.github.com>
2023-01-12 14:43:38 -0700

    Update main.lua

--- Includes/main.lua
+++ Includes/main.lua
@@ -18,6 +18,25 @@ local L = Roman:GetLocale()
 --   ## Do All The Things!!!
 function Roman:Main()
 end
+
+function Roman:ShowUI()
+  local RomanUI = LibStub("AceGUI-3.0")
+  local RomanFrame = RomanUI:Create("Frame")
+  RomanFrame:SetTitle("Example Frame")
+  RomanFrame:SetStatusText("AceGUI-3.0 Example Container Frame")
+  RomanFrame:SetCallback("OnClose", function(widget) RomanUI:Release(widget) end)
+  RomanFrame:SetLayout("Flow")
+  
+  local RomanFrameEditBox = RomanUI:Create("EditBox")
+  RomanFrameEditBox:SetLabel("Insert text:")
+  RomanFrameEditBox:SetWidth(200)
+  RomanFrame:AddChild(RomanFrameEditBox)
+  
+  local RomanFrameButton = RomanUI:Create("Button")
+  RomanFrameButton:SetText("Click Me!")
+  RomanFrameButton:SetWidth(200)
+  RomanFrame:AddChild(RomanFrameButton)
+end
 --[[
      ########################################################################
      |  Last Editted By: @file-author@ - @file-date-iso@
