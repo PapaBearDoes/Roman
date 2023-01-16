331079eb3cd1d0f77c7489767f86999b57c546f7
PapaBearDoes <73200436+PapaBearDoes@users.noreply.github.com>
2023-01-16 12:32:51 -0700

    Update frame.lua

--- Includes/frame.lua
+++ Includes/frame.lua
@@ -18,32 +18,71 @@ local RomanUI = LibStub("AceGUI-3.0")
 --[[ ######################################################################## ]]
 --   ## Do All The Things!!!
 function Roman:ShowUI()
-  local Frame = RomanUI:Create("Frame")
-  Frame:SetWidth(800)
-  Frame:SetTitle(myName)
-  Frame:SetStatusText(L["MessageBarkerForWoW"])
-  Frame:SetCallback("OnClose", function(widget) RomanUI:Release(widget) end)
-  Frame:SetLayout("Flow")
+  -- function that draws the widgets for the first tab
+  local function DrawGroup1(container)
+    local desc = RomanUI:Create("Label")
+    desc:SetText("This is Tab 1")
+    desc:SetFullWidth(true)
+    container:AddChild(desc)
+    
+    local button = RomanUI:Create("Button")
+    button:SetText("Tab 1 Button")
+    button:SetWidth(200)
+    container:AddChild(button)
+  end
   
-  local LeftFrame = RomanUI:Create("Frame")
-  LeftFrame:SetWidth(200)
-  LeftFrame:SetTitle("Message Type")
-  Frame:AddChild(LeftFrame)
+  -- function that draws the widgets for the second tab
+  local function DrawGroup2(container)
+    local desc = RomanUI:Create("Label")
+    desc:SetText("This is Tab 2")
+    desc:SetFullWidth(true)
+    container:AddChild(desc)
+    
+    local button = RomanUI:Create("Button")
+    button:SetText("Tab 2 Button")
+    button:SetWidth(200)
+    container:AddChild(button)
+  end
   
-  local RightFrame = RomanUI:Create("Frame")
-  RightFrame:SetWidth(600)
-  RightFrame:SetTitle("Message Type")
-  Frame:AddChild(RightFrame)
+  -- Callback function for OnGroupSelected
+  local function SelectGroup(container, event, group)
+    container:ReleaseChildren()
+    if group == "tab1" then
+      DrawGroup1(container)
+    elseif group == "tab2" then
+      DrawGroup2(container)
+    end
+  end
 
-  local EditBox = RomanUI:Create("EditBox")
-  EditBox:SetLabel("Insert text:")
-  EditBox:SetWidth(200)
-  Frame:AddChild(EditBox)
+  -- Create the frame container
+  local frame = RomanUI:Create("Frame")
+  frame:SetTitle("Example Frame")
+  frame:SetStatusText("AceGUI-3.0 Example Container Frame")
+  frame:SetCallback("OnClose", function(widget) RomanUI:Release(widget) end)
+  -- Fill Layout - the TabGroup widget will fill the whole frame
+  frame:SetLayout("Fill")
   
-  local Button = RomanUI:Create("Button")
-  Button:SetText("Click Me!")
-  Button:SetWidth(200)
-  Frame:AddChild(Button)
+  -- Create the TabGroup
+  local tab = RomanUI:Create("TabGroup")
+  tab:SetLayout("Flow")
+  -- Setup which tabs to show
+  tab:SetTabs({
+    {
+      text="Tab 1",
+      value="tab1"
+    },
+    {
+      text="Tab 2",
+      value="tab2"
+    }
+  })
+  -- Register callback
+  tab:SetCallback("OnGroupSelected", SelectGroup)
+  -- Set initial Tab (this will fire the OnGroupSelected callback)
+  tab:SelectTab("tab1")
+  
+  -- add to the frame container
+  frame:AddChild(tab)
 end
 --[[
      ########################################################################
